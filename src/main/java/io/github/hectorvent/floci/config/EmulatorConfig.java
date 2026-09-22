df68eebaeff533c7e7d1746a32e44fd038dd3bf0
luiseterc <ramalama18@gmail.com>
2026-09-22 15:09:42 +0200

    feat(ec2): support an external image catalog (#4144)

--- src/main/java/io/github/hectorvent/floci/config/EmulatorConfig.java
+++ src/main/java/io/github/hectorvent/floci/config/EmulatorConfig.java
@@ -2594,6 +2594,9 @@ public interface EmulatorConfig {
     }
 
     interface Ec2ServiceConfig {
+        /** Optional full EC2 catalog file for locally built guest images. */
+        Optional<String> imageCatalogPath();
+
         @WithDefault("true")
         boolean enabled();
 
